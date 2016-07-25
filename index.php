b77b6ffba7b80b39068b36cd1525ccbacb206b8c
Hatim <PGeekC@gmail.com>
2016-07-25 20:43:45 +0000

    Premier Commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="Home Page">
+        <meta name="author" content="Hatim">
+        <title>Custom Your Life - Accueil</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!-- START NAVBAR -->
+        <nav class="navbar navbar-default main" role="navigation">
+            <div class="container">
+        	<!-- Brand and toggle get grouped for better mobile display -->
+        	<div class="navbar-header">
+        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        			<span class="sr-only">Toggle navigation</span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+        		</button>
+        		<a class="navbar-brand" href="">Star's Style</a>
+        	</div>
+
+        	<!-- Collect the nav links, forms, and other content for toggling -->
+        	<div class="collapse navbar-collapse navbar-ex1-collapse">
+        		<ul class="nav navbar-nav">
+        			<li><a href="#">Accueil</a></li>
+        			<li><a href="#">Homme</a></li>
+        			<li><a href="#">Femme</a></li>
+        			<li><a href="#">Accessoire</a></li>
+        			<li class="active"><a href="#">Créer mon propre t-shirt</a></li>
+        		</ul>
+
+        		<ul class="nav navbar-nav navbar-right">
+        			<li><a href="#">Link</a></li>
+        			<li class="dropdown">
+        				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        				<ul class="dropdown-menu">
+        					<li><a href="#">Action</a></li>
+        					<li><a href="#">Another action</a></li>
+        					<li><a href="#">Something else here</a></li>
+        					<li><a href="#">Separated link</a></li>
+        				</ul>
+        			</li>
+        		</ul>
+        	</div><!-- /.navbar-collapse -->
+            </div>
+        </nav>
+        <!-- END NAVBAR -->
+
+		<!-- START HEADER -->
+		<header class="main">
+			<div id="container">
+                <img src="images/bg.png" alt="" class="img-responsive" width="100%">
+                <div class="jumbotron">
+                	<div class="container">
+                		<h1>DES T-SHIRT A VOTRE STYLE ET A VOTRE MESURE ! </h1>
+                		<p>Marre d'acheter des vêtements qui ne vous conviennent pas. des couleurs qui ne vous plaisent pas, des vêtements qui ne sont pas résistants? </p>
+                		<p>
+                			<a class="btn btn-primary btn-lg">Créer votre t-shirt maintenant !</a>
+                		</p>
+                	</div>
+                </div><!-- END JUMBOTRON-->
+            </div>
+		</header>
+		<!-- END HEADER -->
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
